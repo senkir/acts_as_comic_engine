f873b535c7f318124b978fdce3fd81567800dbff
travis <travis@crowdcompass.com>
2011-06-15 23:33:30 -0700

    set up blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spidermuffin::Application.config.secret_token = '26451a2a150d97d5a2d98b8a2edc996c71f53249979def2507b0a85f7dbfa00a60430216a3270114a19e26645f73e72a3548227e7d1497e28cab013a045064b3'
