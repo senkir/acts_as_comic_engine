813fa6493fb45da130184ff31a91512d4333ccc8
travis <churchill.travis@gmail.com>
2011-06-29 01:27:03 -0700

    updated forms for pages to better represent the relationship with comics.

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -12,7 +12,12 @@ ActiveAdmin.register Page do
     column "Title" do |page|
       link_to page.title, admin_page_path(page.id)
     end
-    column :comic_id
+    column "preview" do |preview|
+      image_tag(preview.image.thumb.url)
+    end
+    column "comic" do |page|
+      page.comic.name
+    end
     column :sequence
   end
   
