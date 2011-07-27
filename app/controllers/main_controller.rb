7b9f0b6d448d03d1bd6b6874c05e906cddc7e64b
travis <churchill.travis@gmail.com>
2011-07-27 01:00:12 -0700

    working on style sheets and setting up the main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
   
   def index
     #do stuff for the main controller
-    @page = nil
+    @enable_featured_comic = Setting.enabled_for_name "enable_featured_comic"
   end
 end
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,17 @@
 class Setting < ActiveRecord::Base
   validates_presence_of :setting_name, :setting_value  
+
+  def self.value_for_name name
+    @setting = Setting.find_by_setting_name(name)
+    @setting.setting_value
+  end
+  
+  def self.enabled_for_name name
+    result = self.value_for_name name
+    if result == true || result == "1"
+      true
+    else
+      false
+    end
+  end
 end
